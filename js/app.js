b4087ec89b07138bd7c3aa26c210a570353ffaea
Marcelo Antonio Pereira Marcolino <marcelo.marcolino.dev@gmil.com>
2025-05-16 21:05:31 -0300

    Calculando valor total do carrinho

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+let totalGeral;
+limpar();
+
 function adicionar(){
     // recuperar valores nome do produto, quantidade e valor
     let produto = document.getElementById('produto').value;
@@ -10,13 +13,19 @@ function adicionar(){
 
     // adicionar no carrinho
     let carrinho = document.getElementById('lista-produtos');
-    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+    carrinho.innerHTML += `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$ ${preco},00</span>
         </section>`;
 
     // atualizar o valor total
+    totalGeral += preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral},00`;
+    document.getElementById('quantidade').value = 0;
 }
 
 function limpar(){
-
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0,00';
 }
